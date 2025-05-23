c0ac8895bf4ad4df068d6366e6d8bb8b85b475d7
NgirfanulMangali <qwertymetal0.com>
2025-05-22 22:51:08 -0700

    done

--- dist/npm-project.1fcc916e.js
+++ dist/npm-project.1fcc916e.js
@@ -672,20 +672,43 @@ var _dayjs = require("dayjs");
 var _dayjsDefault = parcelHelpers.interopDefault(_dayjs);
 var _micromodal = require("micromodal");
 var _micromodalDefault = parcelHelpers.interopDefault(_micromodal);
+var _utc = require("dayjs/plugin/utc");
+var _utcDefault = parcelHelpers.interopDefault(_utc);
+var _timezone = require("dayjs/plugin/timezone");
+var _timezoneDefault = parcelHelpers.interopDefault(_timezone);
+(0, _dayjsDefault.default).extend((0, _utcDefault.default));
+(0, _dayjsDefault.default).extend((0, _timezoneDefault.default));
 (0, _micromodalDefault.default).init();
-function updateTime() {
-    const currentTime = (0, _dayjsDefault.default)().format('h:mm:ss A');
-    document.getElementById("timeNow").textContent = currentTime;
-}
-updateTime();
-setInterval(updateTime, 1000);
 const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
 document.getElementById("country").textContent = timeZoneName;
 const dateUser = (0, _dayjsDefault.default)().format("dddd,D MMMM,YYYY");
 document.getElementById("date").textContent = dateUser;
-document.getElementById("edit");
+// modal
+const form = document.getElementById('timezone-form');
+const result = document.getElementById('country');
+const zone = document.getElementById('timeNow');
+let intervalId;
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const selectedZone = form.timezone.value;
+    result.textContent = `${selectedZone}`;
+    if (intervalId) clearInterval(intervalId);
+    function time() {
+        const now = (0, _dayjsDefault.default)().tz(selectedZone).format('hh:mm:ss A');
+        zone.textContent = now;
+    }
+    time();
+    intervalId = setInterval(time, 1000);
+});
+const zone2 = document.getElementById('timeNow');
+function time() {
+    const now = (0, _dayjsDefault.default)().format('hh:mm:ss A');
+    zone2.textContent = now;
+}
+time();
+intervalId = setInterval(time, 1000);
 
-},{"dayjs":"7jGxJ","micromodal":"fpXyP","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}],"7jGxJ":[function(require,module,exports,__globalThis) {
+},{"dayjs":"7jGxJ","micromodal":"fpXyP","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT","dayjs/plugin/utc":"g0vMZ","dayjs/plugin/timezone":"7hEII"}],"7jGxJ":[function(require,module,exports,__globalThis) {
 !function(t, e) {
     module.exports = e();
 }(this, function() {
@@ -1285,6 +1308,189 @@ exports.export = function(dest, destName, get) {
         get: get
     });
 };
+
+},{}],"g0vMZ":[function(require,module,exports,__globalThis) {
+!function(t, i) {
+    module.exports = i();
+}(this, function() {
+    "use strict";
+    var t = "minute", i = /[+-]\d\d(?::?\d\d)?/g, e = /([+-]|\d\d)/g;
+    return function(s, f, n) {
+        var u = f.prototype;
+        n.utc = function(t) {
+            var i = {
+                date: t,
+                utc: !0,
+                args: arguments
+            };
+            return new f(i);
+        }, u.utc = function(i) {
+            var e = n(this.toDate(), {
+                locale: this.$L,
+                utc: !0
+            });
+            return i ? e.add(this.utcOffset(), t) : e;
+        }, u.local = function() {
+            return n(this.toDate(), {
+                locale: this.$L,
+                utc: !1
+            });
+        };
+        var o = u.parse;
+        u.parse = function(t) {
+            t.utc && (this.$u = !0), this.$utils().u(t.$offset) || (this.$offset = t.$offset), o.call(this, t);
+        };
+        var r = u.init;
+        u.init = function() {
+            if (this.$u) {
+                var t = this.$d;
+                this.$y = t.getUTCFullYear(), this.$M = t.getUTCMonth(), this.$D = t.getUTCDate(), this.$W = t.getUTCDay(), this.$H = t.getUTCHours(), this.$m = t.getUTCMinutes(), this.$s = t.getUTCSeconds(), this.$ms = t.getUTCMilliseconds();
+            } else r.call(this);
+        };
+        var a = u.utcOffset;
+        u.utcOffset = function(s, f) {
+            var n = this.$utils().u;
+            if (n(s)) return this.$u ? 0 : n(this.$offset) ? a.call(this) : this.$offset;
+            if ("string" == typeof s && (s = function(t) {
+                void 0 === t && (t = "");
+                var s = t.match(i);
+                if (!s) return null;
+                var f = ("" + s[0]).match(e) || [
+                    "-",
+                    0,
+                    0
+                ], n = f[0], u = 60 * +f[1] + +f[2];
+                return 0 === u ? 0 : "+" === n ? u : -u;
+            }(s), null === s)) return this;
+            var u = Math.abs(s) <= 16 ? 60 * s : s, o = this;
+            if (f) return o.$offset = u, o.$u = 0 === s, o;
+            if (0 !== s) {
+                var r = this.$u ? this.toDate().getTimezoneOffset() : -1 * this.utcOffset();
+                (o = this.local().add(u + r, t)).$offset = u, o.$x.$localOffset = r;
+            } else o = this.utc();
+            return o;
+        };
+        var h = u.format;
+        u.format = function(t) {
+            var i = t || (this.$u ? "YYYY-MM-DDTHH:mm:ss[Z]" : "");
+            return h.call(this, i);
+        }, u.valueOf = function() {
+            var t = this.$utils().u(this.$offset) ? 0 : this.$offset + (this.$x.$localOffset || this.$d.getTimezoneOffset());
+            return this.$d.valueOf() - 6e4 * t;
+        }, u.isUTC = function() {
+            return !!this.$u;
+        }, u.toISOString = function() {
+            return this.toDate().toISOString();
+        }, u.toString = function() {
+            return this.toDate().toUTCString();
+        };
+        var l = u.toDate;
+        u.toDate = function(t) {
+            return "s" === t && this.$offset ? n(this.format("YYYY-MM-DD HH:mm:ss:SSS")).toDate() : l.call(this);
+        };
+        var c = u.diff;
+        u.diff = function(t, i, e) {
+            if (t && this.$u === t.$u) return c.call(this, t, i, e);
+            var s = this.local(), f = n(t).local();
+            return c.call(s, f, i, e);
+        };
+    };
+});
+
+},{}],"7hEII":[function(require,module,exports,__globalThis) {
+!function(t, e) {
+    module.exports = e();
+}(this, function() {
+    "use strict";
+    var t = {
+        year: 0,
+        month: 1,
+        day: 2,
+        hour: 3,
+        minute: 4,
+        second: 5
+    }, e = {};
+    return function(n, i, o) {
+        var r, a = function(t, n, i) {
+            void 0 === i && (i = {});
+            var o = new Date(t), r = function(t, n) {
+                void 0 === n && (n = {});
+                var i = n.timeZoneName || "short", o = t + "|" + i, r = e[o];
+                return r || (r = new Intl.DateTimeFormat("en-US", {
+                    hour12: !1,
+                    timeZone: t,
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    timeZoneName: i
+                }), e[o] = r), r;
+            }(n, i);
+            return r.formatToParts(o);
+        }, u = function(e, n) {
+            for(var i = a(e, n), r = [], u = 0; u < i.length; u += 1){
+                var f = i[u], s = f.type, m = f.value, c = t[s];
+                c >= 0 && (r[c] = parseInt(m, 10));
+            }
+            var d = r[3], l = 24 === d ? 0 : d, h = r[0] + "-" + r[1] + "-" + r[2] + " " + l + ":" + r[4] + ":" + r[5] + ":000", v = +e;
+            return (o.utc(h).valueOf() - (v -= v % 1e3)) / 6e4;
+        }, f = i.prototype;
+        f.tz = function(t, e) {
+            void 0 === t && (t = r);
+            var n, i = this.utcOffset(), a = this.toDate(), u = a.toLocaleString("en-US", {
+                timeZone: t
+            }), f = Math.round((a - new Date(u)) / 1e3 / 60), s = 15 * -Math.round(a.getTimezoneOffset() / 15) - f;
+            if (!Number(s)) n = this.utcOffset(0, e);
+            else if (n = o(u, {
+                locale: this.$L
+            }).$set("millisecond", this.$ms).utcOffset(s, !0), e) {
+                var m = n.utcOffset();
+                n = n.add(i - m, "minute");
+            }
+            return n.$x.$timezone = t, n;
+        }, f.offsetName = function(t) {
+            var e = this.$x.$timezone || o.tz.guess(), n = a(this.valueOf(), e, {
+                timeZoneName: t
+            }).find(function(t) {
+                return "timezonename" === t.type.toLowerCase();
+            });
+            return n && n.value;
+        };
+        var s = f.startOf;
+        f.startOf = function(t, e) {
+            if (!this.$x || !this.$x.$timezone) return s.call(this, t, e);
+            var n = o(this.format("YYYY-MM-DD HH:mm:ss:SSS"), {
+                locale: this.$L
+            });
+            return s.call(n, t, e).tz(this.$x.$timezone, !0);
+        }, o.tz = function(t, e, n) {
+            var i = n && e, a = n || e || r, f = u(+o(), a);
+            if ("string" != typeof t) return o(t).tz(a);
+            var s = function(t, e, n) {
+                var i = t - 60 * e * 1e3, o = u(i, n);
+                if (e === o) return [
+                    i,
+                    e
+                ];
+                var r = u(i -= 60 * (o - e) * 1e3, n);
+                return o === r ? [
+                    i,
+                    o
+                ] : [
+                    t - 60 * Math.min(o, r) * 1e3,
+                    Math.max(o, r)
+                ];
+            }(o.utc(t, i).valueOf(), f, a), m = s[0], c = s[1], d = o(m).utcOffset(c);
+            return d.$x.$timezone = a, d;
+        }, o.tz.guess = function() {
+            return Intl.DateTimeFormat().resolvedOptions().timeZone;
+        }, o.tz.setDefault = function(t) {
+            r = t;
+        };
+    };
+});
 
 },{}]},["iUuJv","fILKw"], "fILKw", "parcelRequire2372", {})
 
--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 import dayjs from 'dayjs';
 import MicroModal from 'micromodal';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 MicroModal.init();
-
-
-
-function updateTime() {
-    const currentTime = dayjs().format('h:mm:ss A')
-    document.getElementById("timeNow").textContent = currentTime;
-}
-updateTime();
-
-setInterval(updateTime, 1000);
 
 const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
 document.getElementById("country").textContent = timeZoneName;
@@ -20,4 +15,38 @@ const dateUser = dayjs().format("dddd,D MMMM,YYYY")
 document.getElementById("date").textContent = dateUser;
 
 
-document.getElementById("edit")
+
+// modal
+
+const form = document.getElementById('timezone-form');
+const result = document.getElementById('country');
+const zone = document.getElementById('timeNow');
+
+let intervalId;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const selectedZone = form.timezone.value;
+    result.textContent = `${selectedZone}`;
+
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+
+
+    function time() {
+        const now = dayjs().tz(selectedZone).format('hh:mm:ss A');
+        zone.textContent = now;
+    }
+    time();
+    intervalId = setInterval(time, 1000);
+});
+
+
+const zone2 = document.getElementById('timeNow');
+function time() {
+    const now = dayjs().format('hh:mm:ss A');
+    zone2.textContent = now;
+}
+time();
+intervalId = setInterval(time, 1000);
